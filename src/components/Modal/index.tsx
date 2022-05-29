23ac824c485a8ec2745e326f1eac2361d9cc0788
Alexandre Lira <alexandrelira112@gmail.com>
2022-05-29 11:49:24 -0300

    refactoring Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,34 +1,21 @@
-import { Component } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  children: ReactNode;
+}
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
+function Modal(props: ModalProps) {
+  const { isOpen, setIsOpen,children } = props
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
 
     return (
       <ReactModal
         shouldCloseOnOverlayClick={!false}
         onRequestClose={setIsOpen}
-        isOpen={modalStatus}
+        isOpen={isOpen}
         ariaHideApp={false}
         style={{
           content: {
@@ -52,7 +39,7 @@ class Modal extends Component {
         {children}
       </ReactModal>
     );
-  }
+
 };
 
 export default Modal;
